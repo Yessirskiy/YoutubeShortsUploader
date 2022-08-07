2abb581a1b9515e1571b89deb34e4346174e780e
KolyaDobrydnev <nidobrydnev@gmail.com>
2022-08-07 23:30:03 +0300

    Update/fixing interaction with YouTube methods.

--- login.py
+++ login.py
@@ -1,21 +1,76 @@
 import json
+from typing import Dict, List
 
-secrets = {
-    "web": {
-    "client_id": None,
-    "client_secret": None,
-    "redirect_uris": [],
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://accounts.google.com/o/oauth2/token"
-}}
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
-if __name__ == "__main__":
-    name = input("Enter name of the account: ")
-    client_id = input("Enter client id from google console API: ")
-    client_secret = input("Enter client secret from google console API: ")
-    secrets['web']['client_id'] = client_id
-    secrets['web']['client_secret'] = client_secret
+from loguru import logger
 
-    with open(f'client_secrets_{name}.json', 'w') as file: 
-        json.dump(secrets, file, ensure_ascii=True, indent=4)
-    print(f"Succesfully save secrets to client_secrets_{name}.json file")
+def domainToURL(domain: str) -> str:
+    """ 
+    Converts a domain to the valid URL 
+
+    Parameters
+    ----------
+        domain (str): Partial domain
+
+    Returns
+    -------
+        url (str): Valid URL
+    """
+    if domain.startswith("."):
+        domain = "www" + domain
+    return "http://" + domain
+
+def login(driver: WebDriver, cookies_file: str, account: str) -> bool:
+    """
+    Loggin in account using cookies.
+
+    Parameters
+    ---------- 
+        driver (WebDriver): Selenium WebDriver to access from
+        cookies_file (str): Path to the file with cookies of account
+
+    Returns
+    -------
+        logged_in (bool): True - succesfully logged in, False - didn't log in
+
+    Restore auth cookies from a file. Does not guarantee that the user is logged in afterwards.
+    Visits the domains specified in the cookies to set them, the previous page is not restored.
+    """
+
+    logger.info(f"Logging in {account}'s account")
+
+    domain_cookies: Dict[str, List[object]] = {}
+
+    with open(cookies_file) as file:
+        cookies: List = json.load(file)
+        # Sort cookies by domain, because we need to visit to domain to add cookies
+        for cookie in cookies:
+            try:
+                domain_cookies[cookie["domain"]].append(cookie)
+            except KeyError:
+                domain_cookies[cookie["domain"]] = [cookie]
+
+    for domain, cookies in domain_cookies.items():
+        driver.get(domainToURL(domain + "/robots.txt"))
+        for cookie in cookies:
+            cookie.pop("sameSite", None) 
+            cookie.pop("storeId", None)  
+            try:
+                driver.add_cookie(cookie)
+            except:
+                logger.error(f"Couldnt's set cookie {cookie['name']} for {domain}")
+
+    driver.get("https://www.youtube.com")
+    assert "YouTube" in driver.title # Checks if YouTube opened
+
+    try:
+        WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.ID, "avatar-btn")))
+        logger.success(f'Succesfully logged in {account} account.')
+        return True
+    except TimeoutError:
+        logger.error(f"Couldn't log in {account} account. Please, update cookies of this account and try again.")
+        return False    
--- upload.py
+++ upload.py
@@ -1,176 +1,233 @@
-#!/usr/bin/python
-
-import httplib2
+import logging
+import re
+from datetime import datetime
+from time import sleep
 import os
-import random
-import sys
-import time
 
-from apiclient.discovery import build
-from apiclient.errors import HttpError
-from apiclient.http import MediaFileUpload
-from oauth2client.client import flow_from_clientsecrets
-from oauth2client.file import Storage
-from oauth2client.tools import argparser, run_flow
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException
 
+from loguru import logger
+import config
+import emoji
 
-# Explicitly tell the underlying HTTP transport library not to retry, since
-# we are handling retry logic ourselves.
-httplib2.RETRIES = 1
+JS_ADD_TEXT_TO_INPUT = '''
+const text = `{content}`;
+const dataTransfer = new DataTransfer();
+dataTransfer.setData('text', text);
+const event = new ClipboardEvent('paste', {{
+  clipboardData: dataTransfer,
+  bubbles: true
+}});
+arguments[0].dispatchEvent(event)
+'''
 
-# Maximum number of times to retry before giving up.
-MAX_RETRIES = 10
-
-# Always retry when these exceptions are raised.
-RETRIABLE_EXCEPTIONS = (httplib2.HttpLib2Error, IOError)
-
-# Always retry when an apiclient.errors.HttpError with one of these status
-# codes is raised.
-RETRIABLE_STATUS_CODES = [500, 502, 503, 504]
-
-# The CLIENT_SECRETS_FILE variable specifies the name of a file that contains
-# the OAuth 2.0 information for this application, including its client_id and
-# client_secret. You can acquire an OAuth 2.0 client ID and client secret from
-# the Google API Console at
-# https://console.developers.google.com/.
-# Please ensure that you have enabled the YouTube Data API for your project.
-# For more information about using OAuth2 to access the YouTube Data API, see:
-#   https://developers.google.com/youtube/v3/guides/authentication
-# For more information about the client_secrets.json file format, see:
-#   https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
-CLIENT_SECRETS_FILE = "client_secrets_nidob.json"
-
-# This OAuth 2.0 access scope allows an application to upload files to the
-# authenticated user's YouTube channel, but doesn't allow other types of access.
-YOUTUBE_UPLOAD_SCOPE = "https://www.googleapis.com/auth/youtube.upload"
-YOUTUBE_API_SERVICE_NAME = "youtube"
-YOUTUBE_API_VERSION = "v3"
-
-# This variable defines a message to display if the CLIENT_SECRETS_FILE is
-# missing.
-MISSING_CLIENT_SECRETS_MESSAGE = """
-WARNING: Please configure OAuth 2.0
-
-To make this sample run you will need to populate the client_secrets.json file
-found at:
-
-   %s
-
-with information from the API Console
-https://console.developers.google.com/
-
-For more information about the client_secrets.json file format, please visit:
-https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
-""" % os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                   CLIENT_SECRETS_FILE))
-
-VALID_PRIVACY_STATUSES = ("public", "private", "unlisted")
-
-
-def get_authenticated_service(args):
-    flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE,
-        scope=YOUTUBE_UPLOAD_SCOPE,
-        message=MISSING_CLIENT_SECRETS_MESSAGE)
-
-    storage = Storage("%s-oauth2.json" % sys.argv[0])
-    credentials = storage.get()
-
-    if credentials is None or credentials.invalid:
-        credentials = run_flow(flow, storage, args)
-
-    return build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
-        http=credentials.authorize(httplib2.Http()))
-
-def initialize_upload(youtube, options):
-    tags = None
-    if options.keywords:
-        tags = options.keywords.split(",")
-
-    body=dict(
-        snippet=dict(
-        title=options.title,
-        description=options.description,
-        tags=tags,
-        categoryId=options.category
-        ),
-        status=dict(
-        privacyStatus=options.privacyStatus
-        )
-    )
-
-    # Call the API's videos.insert method to create and upload the video.
-    insert_request = youtube.videos().insert(
-        part=",".join(body.keys()),
-        body=body,
-        # The chunksize parameter specifies the size of each chunk of data, in
-        # bytes, that will be uploaded at a time. Set a higher value for
-        # reliable connections as fewer chunks lead to faster uploads. Set a lower
-        # value for better recovery on less reliable connections.
-        #
-        # Setting "chunksize" equal to -1 in the code below means that the entire
-        # file will be uploaded in a single HTTP request. (If the upload fails,
-        # it will still be retried where it left off.) This is usually a best
-        # practice, but if you're using Python older than 2.6 or if you're
-        # running on App Engine, you should set the chunksize to something like
-        # 1024 * 1024 (1 megabyte).
-        media_body=MediaFileUpload(options.file, chunksize=-1, resumable=True)
-    )
-
-    resumable_upload(insert_request)
-
-# This method implements an exponential backoff strategy to resume a
-# failed upload.
-def resumable_upload(insert_request):
-  response = None
-  error = None
-  retry = 0
-  while response is None:
+def uploadFile(
+        driver: WebDriver,
+        video_path: str,
+        title: str,
+        account: str,
+        description: str = None,
+        game: str = None,
+        kids: bool = False,
+        thumbnail_path: str = None) -> bool:
+    
+    driver.get('https://studio.youtube.com/')
+    # Clicking on Upload Button
     try:
-        print("Uploading file...")
-        status, response = insert_request.next_chunk()
-        if response is not None:
-            if 'id' in response:
-                print(f"Video id '{response['id']}' was successfully uploaded.")
-            else:
-                exit(f"The upload failed with an unexpected response: {response}")
-    except HttpError as e:
-        if e.resp.status in RETRIABLE_STATUS_CODES:
-            error = "A retriable HTTP error %d occurred:\n%s" % (e.resp.status, e.content)
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "ytcp-button#create-icon"))).click()
+        WebDriverWait(driver, 20).until(
+            EC.element_to_be_clickable((By.XPATH, '//tp-yt-paper-item[@test-id="upload-beta"]'))
+        ).click()
+    except Exception as e:
+        logger.error("There was an error while trying to click on 'Upload' button. Please, make sure you have all the guides completed.")
+        driver.close()
+        return False
+
+    # Uploading Video File
+    try:
+        video_input = driver.find_element("xpath", '//input[@type="file"]')
+        video_input.send_keys(os.path.abspath(video_path))
+    except Exception as e:
+        logger.error("There was an error while uploading file. Please, make sure path to the file with video is correct.")
+        driver.close()
+        return False
+
+    logger.success("File succesfully loaded on website. Now setting up video's details.")
+
+    basic_setup = _set_basic_settings(driver, title, description, thumbnail_path)
+    advance_setup = _set_advanced_settings(driver, game, kids)
+
+
+    # Go to visibility settings
+    for i in range(3):
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "next-button"))).click()
+
+    processed = _wait_for_processing(driver)
+
+    try:
+        if basic_setup and advance_setup and processed:
+            driver.find_element(By.XPATH, '/html/body/ytcp-uploads-dialog/tp-yt-paper-dialog/div/ytcp-animatable[1]/ytcp-uploads-review/div[2]/div[1]/ytcp-video-visibility-select/div[1]/tp-yt-paper-radio-group/tp-yt-paper-radio-button[3]').click()
+            sleep(3) 
         else:
-            raise
-    except RETRIABLE_EXCEPTIONS as e:
-      error = "A retriable error occurred: %s" % e
+            return False
+    except Exception as e:
+        logger.error("Error while setting up access to the video.")
+        driver.close()
+        return False
 
-    if error is not None:
-        print(error)
-        retry += 1
-        if retry > MAX_RETRIES:
-            exit("No longer attempting to retry.")
+    # Upload
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "done-button"))).click()
+    sleep(5) # Wait for final screen to disappear
+    logger.success(f"Video '{title}' succesfully uploaded on {account}'s account.")
+    return True
 
-        max_sleep = 2 ** retry
-        sleep_seconds = random.random() * max_sleep
-        print("Sleeping %f seconds and then retrying..." % sleep_seconds)
-        time.sleep(sleep_seconds)
-
-if __name__ == '__main__':
-    argparser.add_argument("--file", required=True, help="Video file to upload")
-    argparser.add_argument("--title", help="Video title", default="Test Title")
-    argparser.add_argument("--description", help="Video description",
-        default="Test Description")
-    argparser.add_argument("--category", default="22",
-        help="Numeric video category. " +
-        "See https://developers.google.com/youtube/v3/docs/videoCategories/list")
-    argparser.add_argument("--keywords", help="Video keywords, comma separated",
-        default="")
-    argparser.add_argument("--privacyStatus", choices=VALID_PRIVACY_STATUSES,
-        default=VALID_PRIVACY_STATUSES[0], help="Video privacy status.")
-    args = argparser.parse_args()
-
-    if not os.path.exists(args.file):
-        exit("Please specify a valid file using the --file= parameter.")
-
-    youtube = get_authenticated_service(args)
+def _wait_for_processing(driver: WebDriver) -> bool:
     try:
-        initialize_upload(youtube, args)
-    except HttpError as e:
-        print("An HTTP error %d occurred:\n%s" % (e.resp.status, e.content))
+        # Wait for processing to complete
+        progress_label: WebElement = driver.find_element(By.CSS_SELECTOR, "span.progress-label")
+        #pattern = re.compile(r"(finished processing)|(processing hd.*)|(check.*)")
+        pattern = re.compile(fr"({config.PROCESSING_FINISHED_REGEXP})")
+        current_progress = progress_label.get_attribute("textContent")
+        last_progress = None
+        while not pattern.match(current_progress.lower()): # Checking if file is fully submitted
+            if last_progress != current_progress:
+                logging.info(f'Current progress: {current_progress}')
+            last_progress = current_progress
+            sleep(5)
+            current_progress = progress_label.get_attribute("textContent")
+            if current_progress == '':
+                logger.error("Video cannot be uploaded on this acc. Maybe, you have reached a limit.")
+                driver.close()
+                return False
+
+        return True
+    except Exception as e:
+        logger.error('Error while awaiting for processing the video.')
+        driver.close()
+        return False
+
+def _set_basic_settings(driver: WebDriver, title: str|None, description: str|None, thumbnail_path: str|None) -> bool:
+    try:
+        title_input: WebElement = WebDriverWait(driver, 60).until(
+            EC.element_to_be_clickable(
+                (
+                    By.XPATH,
+                    '/html/body/ytcp-uploads-dialog/tp-yt-paper-dialog/div/ytcp-animatable[1]/ytcp-ve/ytcp-video-metadata-editor/div/ytcp-video-metadata-editor-basics/div[1]/ytcp-social-suggestions-textbox/ytcp-form-input-container/div[1]/div[2]/div/ytcp-social-suggestion-input/div',
+
+                )
+            )
+        ) # Waiting until file is loaded
+
+        title_input: WebElement = driver.find_element(
+            By.XPATH,
+            '/html/body/ytcp-uploads-dialog/tp-yt-paper-dialog/div/ytcp-animatable[1]/ytcp-ve/ytcp-video-metadata-editor/div/ytcp-video-metadata-editor-basics/div[1]/ytcp-social-suggestions-textbox/ytcp-form-input-container/div[1]/div[2]/div/ytcp-social-suggestion-input/div'
+        )
+        description_input: WebElement = driver.find_element(
+        By.XPATH, '/html/body/ytcp-uploads-dialog/tp-yt-paper-dialog/div/ytcp-animatable[1]/ytcp-ve/ytcp-video-metadata-editor/div/ytcp-video-metadata-editor-basics/div[2]/ytcp-social-suggestions-textbox/ytcp-form-input-container/div[1]/div[2]/div/ytcp-social-suggestion-input/div'
+        )
+        thumbnail_input: WebElement = driver.find_element(
+            By.CSS_SELECTOR,
+            "input#file-loader"
+        )
+
+        if title:
+            title_input.clear()
+            if emoji.emoji_count(title) > 0: # If emoji in title
+                driver.execute_script("arguments[0].innerHTML = '{}'".format(title), title_input)
+            else:
+                title_input.send_keys(title)
+
+        if description:
+            if emoji.emoji_count(description) > 0: # If emoji in description
+                driver.execute_script("arguments[0].innerHTML = '{}'".format(description), description_input)
+            else:
+                description_input.send_keys(description)
+
+        if thumbnail_path:
+            thumbnail_input.send_keys(thumbnail_path)
+        return True
+    except Exception as e:
+        logger.error('Error occured while setting up Title, Description or Thumbnail of the video.')
+        driver.close()
+        return False
+
+def _set_advanced_settings(driver: WebDriver, game_title: str, made_for_kids: bool) -> bool:
+    try:
+        # Open advanced options
+        driver.find_element(By.CSS_SELECTOR, "#toggle-button").click()
+        if game_title:
+            game_title_input: WebElement = driver.find_element(
+                By.CSS_SELECTOR,
+                ".ytcp-form-gaming > "
+                "ytcp-dropdown-trigger:nth-child(1) > "
+                ":nth-child(2) > div:nth-child(3) > input:nth-child(3)"
+            )
+            game_title_input.send_keys(game_title)
+
+            # Select first item in game drop down
+            WebDriverWait(driver, 20).until(
+                EC.element_to_be_clickable(
+                    (
+                        By.CSS_SELECTOR,
+                        "#text-item-2",  # The first item is an empty item
+                    )
+                )
+            ).click()
+
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable(
+            (By.NAME, "VIDEO_MADE_FOR_KIDS_MFK" if made_for_kids else "VIDEO_MADE_FOR_KIDS_NOT_MFK")
+        )).click()
+        return True
+    except Exception as e:
+        logger.error("Error while setting GameTitle or Kids Restrictions.")
+        driver.close()
+        return False
+
+# Functions down below are not used in a project
+
+def _set_endcard(driver: WebDriver):
+    # Add endscreen
+    driver.find_element(By.CSS_SELECTOR, "#endscreens-button").click()
+    sleep(5)
+
+    for i in range(1, 11):
+        try:
+            # Select endcard type from last video or first suggestion if no prev. video
+            driver.find_element(By.CSS_SELECTOR, "div.card:nth-child(1)").click()
+            break
+        except (NoSuchElementException, ElementNotInteractableException):
+            logging.warning(f"Couldn't find endcard button. Retry in 5s! ({i}/10)")
+            sleep(5)
+
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "save-button"))).click()
+
+def _set_time(driver: WebDriver, upload_time: datetime):
+    # Start time scheduling
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.NAME, "SCHEDULE"))).click()
+
+    # Open date_picker
+    driver.find_element(By.CSS_SELECTOR, "#datepicker-trigger > ytcp-dropdown-trigger:nth-child(1)").click()
+
+    date_input: WebElement = driver.find_element(By.CSS_SELECTOR, "input.tp-yt-paper-input")
+    date_input.clear()
+    # Transform date into required format: Mar 19, 2021
+    date_input.send_keys(upload_time.strftime("%b %d, %Y"))
+    date_input.send_keys(Keys.RETURN)
+
+    # Open time_picker
+    driver.find_element(
+        By.CSS_SELECTOR, 
+        "#time-of-day-trigger > ytcp-dropdown-trigger:nth-child(1) > div:nth-child(2)"
+    ).click()
+
+    time_list = driver.find_element(By.CSS_SELECTOR, "tp-yt-paper-item.tp-yt-paper-item")
+    # Transform time into required format: 8:15 PM
+    time_str = upload_time.strftime("%I:%M %p").strip("0")
+    time = [time for time in time_list[2:] if time.text == time_str][0]
+    time.click()
