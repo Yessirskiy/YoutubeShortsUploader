a1ac57710d46da74680c2d352807443f8d5bb4f1
KolyaDobrydnev <nidobrydnev@gmail.com>
2022-08-08 00:04:04 +0300

    Not in use anymore

--- setup.py
+++
@@ -1,8 +0,0 @@
-import os
-import config
-
-if __name__ == '__main__':
-    if not os.path.exists(config.ACCOUNTS_FOLDER):
-        os.mkdir(config.ACCOUNTS_FOLDER)
-    if not os.path.exists(config.VIDEOS_FOLDER):
-        os.mkdir(config.VIDEOS_FOLDER)
